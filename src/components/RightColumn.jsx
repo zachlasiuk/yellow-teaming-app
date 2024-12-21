592d8e91adf804386b7da2cd8c7b1669250c5111
Zach Lasiuk <zach.lasiuk@arm.com>
2024-12-21 11:31:35 -0600

    adding chatbot interface

--- src/components/RightColumn.jsx
+++ src/components/RightColumn.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Box, Text } from '@chakra-ui/react';
+import ChatbotInterface from './ChatbotInterface';
 
 function RightColumn() {
   return (
-    <Box bg="gray.200" p={4} height="100vh">
-      <Text fontSize="xl">Right Column</Text>
-      <Text>Chatbot goes here</Text>
-
-
-    </Box>
+    <ChatbotInterface />
   );
 }
 
